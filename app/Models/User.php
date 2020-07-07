49f67f69172f20dba426fd94d1b8927796b2458b
vagrant <vagrant@homestead>
2020-07-07 06:33:37 +0000

    删除帖子

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
